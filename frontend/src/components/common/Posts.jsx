e7526823d3e901cb016cb2d6ef4b5e7557116ff4
harshitxrajput <harshitxrajput@gmail.com>
2025-04-04 19:56:58 +0530

    fetching posts for home page

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -1,23 +1,67 @@
 import Post from "./Post";
 import PostSkeleton from "../skeletons/PostSkeleton";
-import { POSTS } from "../../utils/db/dummy";
 
-const Posts = () => {
-	const isLoading = false;
+import { useQuery } from "@tanstack/react-query"
+import { useEffect } from "react";
+
+const Posts = ({ feedType }) => {
+
+	const getPostEndpoint = (feedType) => {
+		switch(feedType){
+			case "forYou":
+				return "/api/post/all";
+			
+			case "following":
+				return "/api/post/all/followingposts";
+			
+			default:
+				return "/api/post/all";
+		}
+	}
+
+	const POST_ENDPOINT = getPostEndpoint(feedType);
+
+	const { data:posts, isLoading, refetch, isRefetching } = useQuery({
+		queryKey: ["posts"],
+		queryFn: async () => {
+			try{
+				const res = await fetch(POST_ENDPOINT, {
+					method: "GET",
+					headers: {
+						"Content-Type": "application/json",
+					}
+				});
+
+				const data = await res.json();
+				if(!res.ok){
+					throw new Error(data.error || "Something went wrong");
+				}
+				
+				return data;
+			}
+			catch(err){
+				throw new Error(err.message);
+			}
+		}
+	})
+
+	useEffect(() => {
+		refetch();
+	}, [feedType])
 
 	return (
 		<>
-			{isLoading && (
+			{(isLoading || isRefetching) && (
 				<div className='flex flex-col justify-center'>
 					<PostSkeleton />
 					<PostSkeleton />
 					<PostSkeleton />
 				</div>
 			)}
-			{!isLoading && POSTS?.length === 0 && <p className='text-center my-4'>No posts in this tab. Switch 👻</p>}
-			{!isLoading && POSTS && (
+			{!isLoading && !isRefetching && posts?.length === 0 && <p className='text-center my-4'>No posts in this tab. Switch 👻</p>}
+			{!isLoading && !isRefetching && posts && (
 				<div>
-					{POSTS.map((post) => (
+					{posts.map((post) => (
 						<Post key={post._id} post={post} />
 					))}
 				</div>
--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -29,7 +29,7 @@ const HomePage = () => {
 				<CreatePost />
 
 				{/* POSTS */}
-				<Posts />
+				<Posts feedType={feedType} />
 			</div>
 		</>
 	);
