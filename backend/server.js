52b80a77892e4fc507166debc68c568cdf74f2f3
harshitxrajput <harshitxrajput@gmail.com>
2025-04-14 01:47:55 +0530

    fixed some errors

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from "path";
 import cookieParser from 'cookie-parser';
 import { v2 as cloudinary } from 'cloudinary';
 import dotenv from 'dotenv';
@@ -19,6 +20,7 @@ import connectDB from './db/connectDB.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
@@ -27,7 +29,15 @@ app.use(cookieParser())
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/post', postRoutes);
-app.use('/api/notification', notificationRoutes)
+app.use('/api/notification', notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("/", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
